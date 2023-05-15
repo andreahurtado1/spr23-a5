abe3359b4c7d65b480a02112d28b22859079d76e
Andrea Hurtado <75586588+andreaax@users.noreply.github.com>
2023-05-15 15:26:34 -0700

    Adds new tests for the program!

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -2,8 +2,13 @@
 from calculator_adapter import run
 
 
-### ADD AT LEAST TWO TESTS HERE!
-
+### STUDENT TESTS
+# checks that program runs for subtraction
+assert run("4 - 2").output == "2"
+# checks that program runs for division
+assert run("10 / 2").output == "5"
+# checks if mod is real
+assert run("2 % 2").exit_status != 0
 
 ###
 
